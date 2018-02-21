68bfa91f811eea0731ad274bd6c248c5aa1c7b57
Nando Vieira <fnando.vieira@gmail.com>
2018-02-21 13:07:18 -0800

    Add message about running generator online.

--- src/scripts/components/App.jsx
+++ src/scripts/components/App.jsx
@@ -3,14 +3,18 @@ import React from "react";
 import ActionSelector from "./ActionSelector";
 import AppHeader from "./AppHeader";
 import AppFooter from "./AppFooter";
+import LiveSiteChecker from "./LiveSiteChecker";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <AppHeader />
-        <ActionSelector />
-        <AppFooter />
+        <LiveSiteChecker />
+        <div className="container">
+          <AppHeader />
+          <ActionSelector />
+          <AppFooter />
+        </div>
       </div>
     );
   }
---
+++ src/scripts/components/LiveSiteChecker.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import pkg from "../../../package.json";
+
+export default class LiveSiteChecker extends React.Component {
+  state = {
+    dismissed: false
+  };
+
+  handleDismiss = () => {
+    this.setState({dismissed: true});
+  };
+
+  render() {
+    const {dismissed} = this.state;
+    const runningLive = !!location.protocol.match(/^https?:/);
+    const url = `https://github.com/fnando/stellar-paperwallet/releases/download/v${pkg.version}/stellar-paperwallet.zip`;
+    const skipRendering = dismissed || !runningLive;
+
+    if (skipRendering) {
+      return null;
+    }
+
+    return (
+      <div className="live-site center">
+        <div>
+          <div>
+            You're running this paper wallet generator off of a live website.
+            Even though all code runs on the client-side, is recommended that
+            you <a href={url}>download the zip file</a> and run it locally from your machine.
+          </div>
+          <p className="mb0">
+            <button onClick={this.handleDismiss}>Dismiss</button>
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
